6fb614dd245a75f200276a7c3c46e345ea5c4116
Its Me Hara <17094719+itsmehara@users.noreply.github.com>
2024-08-16 23:42:04 +0530

    Create __init__.py

---
+++ json2htmlpy/__init__.py
@@ -0,0 +1,14 @@
+"""
+json2htmlpy - A Python package for converting JSON data into HTML tables.
+
+Author: Haranadh
+GitHub: https://github.com/itsmehara/json2htmlpy
+Email: hara2help@gmail.com
+"""
+
+# Import the main function to allow direct access when importing the package
+from .jsontohtml import convert_json_to_html
+
+# You can add additional imports or helper functions here if needed
+
+__version__ = '0.1.0'
