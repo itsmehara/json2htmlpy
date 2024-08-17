4792171a1c0a3ab6edc99ede0564dda043c69fe1
Its Me Hara <17094719+itsmehara@users.noreply.github.com>
2024-08-17 13:09:29 +0530

    build success. updating release.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name="json2htmlpy",  # Package name
-    version="1.0.1",  # Version number
+    version="1.0.2",  # Version number
     description="A simple script to convert JSON to HTML tables.",  # Short description
     long_description=long_description,  # Long description from README
     long_description_content_type="text/markdown",  # Type of long description
